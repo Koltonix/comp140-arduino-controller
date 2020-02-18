ea8a66fa064723af114dcf4b67d00b8506fb123b
Christopher Robertson <christopherprobertson@gmail.com>
2020-02-18 16:49:02 +0000

    - Added Input to the small research project which returns an enum based on what key is selected.

--- rgb-controller-prototype/rgb-controller-prototype.cpp
+++ rgb-controller-prototype/rgb-controller-prototype.cpp
@@ -1,13 +1,66 @@
 #include <iostream>
 #include <string>
+#include <conio.h>
 
 #include "Colour.h"
 #include "Led.h"
 
-int main()
+enum InputType
+{
+	LEFT,
+	RIGHT,
+	EXIT,
+	NONE
+};
+
+bool isRunning;
+InputType currentInput;
+
+void GetInput()
+{
+	if (_kbhit())
+	{
+		switch (_getch())
+		{
+			case 'a':
+				currentInput = InputType(LEFT);
+				break;
+			case 'd':
+				currentInput = InputType(RIGHT);
+				break;
+			case 'x':
+				currentInput = InputType(EXIT);
+				isRunning = false;
+				break;
+			default:
+				currentInput = InputType(NONE);
+				break;
+		}
+	}
+}
+
+void Start()
 {
 	Colour colour = Colour(255, 255, 255);
 	Led led = Led(&colour, 1);
 
 	std::cout << led.colour->r << ", " << led.colour->g << ", " << led.colour->b;
 }
+
+void Update()
+{
+	GetInput();
+	std::cout << currentInput << "\n";
+}
+
+int main()
+{
+	isRunning = true;
+	Start();
+
+	while (isRunning)
+	{
+		Update();
+	}
+}
+
