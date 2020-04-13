f20a5727c980b6bea492bc8b5d1edb4ea17f87d9
Christopher Robertson <christopherprobertson@gmail.com>
2020-04-13 09:48:09 +0100

    - Added the encoder conversion script from RGB string to actual RGB.
    - Added background image and higher resolution circle.
    
    - Created a GameController.cs which currently just debugs the encoder.

---
+++ unity project/rgb-comp140/Assets/rgb-comp140/Scripts/GameController.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    private void Start()
+    {
+        Debug.Log(ColourConverter.DecodeColourString("255,255,255"));
+    }
+}
